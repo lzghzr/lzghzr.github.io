59ddf91939cb982fd241d0202f8954b38c80b863
lzghzr <lzggzr@gmail.com>
2016-07-02 22:47:09 +0800

    测试

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+    if (navigator.userAgent.indexOf('Firefox') === -1)
+        return;
+    const elmcanvas = document.querySelector('canvas');
+    const context = elmcanvas.getContext('2d');
+    const image = new Image();
+    image.src = 'arrow.png';
+    elmcanvas.style.cssText = 'cursor: none;';
+    elmcanvas.width = window.innerWidth;
+    elmcanvas.height = window.innerHeight;
+    elmcanvas.addEventListener('mousemove', (ev) => {
+        if (Math.random() > 0.9) {
+            context.clearRect(0, 0, elmcanvas.width, elmcanvas.height);
+            context.drawImage(image, ev.clientX, ev.clientY, 20, 20);
+        }
+    });
+    elmcanvas.addEventListener('mouseout', (ev) => {
+        context.clearRect(0, 0, elmcanvas.width, elmcanvas.height);
+    });
+    window.onresize = (ev) => {
+        elmcanvas.width = window.innerWidth;
+        elmcanvas.height = window.innerHeight;
+    };
+});
