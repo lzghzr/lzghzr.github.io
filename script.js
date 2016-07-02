b3f7a343d8cb9003978834b2781fc443127eeeae
lzghzr <lzggzr@gmail.com>
2016-07-03 01:09:38 +0800

    让你们改UA

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 window.addEventListener('load', () => {
-    if (navigator.userAgent.indexOf('Firefox') === -1)
+    if (!IsFirefox())
         return;
-    const elmcanvas = document.querySelector('canvas');
-    const context = elmcanvas.getContext('2d');
-    const image = new Image();
-    image.src = 'arrow.png';
-    elmcanvas.style.cssText = 'cursor: none;';
-    elmcanvas.width = window.innerWidth;
-    elmcanvas.height = window.innerHeight;
-    elmcanvas.addEventListener('mousemove', (ev) => {
+    const elmCanvas = document.querySelector('canvas');
+    const context = elmCanvas.getContext('2d');
+    const img = new Image();
+    img.src = 'arrow.png';
+    elmCanvas.style.cssText = 'cursor: none;';
+    elmCanvas.width = window.innerWidth;
+    elmCanvas.height = window.innerHeight;
+    elmCanvas.addEventListener('mousemove', (ev) => {
         if (Math.random() > 0.9) {
-            context.clearRect(0, 0, elmcanvas.width, elmcanvas.height);
-            context.drawImage(image, ev.clientX, ev.clientY, 20, 20);
+            context.clearRect(0, 0, elmCanvas.width, elmCanvas.height);
+            context.drawImage(img, ev.clientX, ev.clientY, 20, 20);
         }
     });
-    elmcanvas.addEventListener('mouseout', (ev) => {
-        context.clearRect(0, 0, elmcanvas.width, elmcanvas.height);
+    elmCanvas.addEventListener('mouseout', (ev) => {
+        context.clearRect(0, 0, elmCanvas.width, elmCanvas.height);
     });
     window.onresize = (ev) => {
-        elmcanvas.width = window.innerWidth;
-        elmcanvas.height = window.innerHeight;
+        elmCanvas.width = window.innerWidth;
+        elmCanvas.height = window.innerHeight;
     };
 });
+function IsFirefox() {
+    const elmVideo = document.createElement('video');
+    return (elmVideo.mozHasAudio === false);
+}
