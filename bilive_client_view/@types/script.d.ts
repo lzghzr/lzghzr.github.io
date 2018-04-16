8735a246b98cbfb5f76e061947000f63e55cadb6
lzghzr <lzggzr@gmail.com>
2018-04-16 19:50:57 +0800

    删除bilive_view

--- bilive_client_view/@types/script.d.ts
+++
@@ -1,9 +0,0 @@
-interface modalOPtions {
-  body: string | DocumentFragment
-  title?: string
-  close?: string
-  ok?: string
-  showOK?: boolean
-  onOK?: (body: this['body']) => void
-  onClose?: (body: this['body']) => void
-}
